7fe70fe7090b544b79807531d5bf38d9cf10f80c
Aniruddh Agarwal <aaniruddh99@gmail.com>
2018-07-05 19:48:02 -0400

    Implement base64ToHex

--- src/set1/Util.hs
+++ src/set1/Util.hs
@@ -1,4 +1,4 @@
-{- LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE NoImplicitPrelude, OverloadedStrings #-}
 
 module Util where
 
@@ -6,7 +6,13 @@ import Protolude
 
 import qualified Data.ByteString.Base16 as Hex
 import qualified Data.ByteString.Base64 as B64
-import Control.Lens
+
+{- Encoding/Decoding utilities -}
 
 hexToBase64 :: ByteString -> ByteString
 hexToBase64 = B64.encode . fst . Hex.decode
+
+base64ToHex :: ByteString -> ByteString
+base64ToHex inp = Hex.encode $ case B64.decode inp of
+                                 Right s -> s
+                                 Left  _ -> ""
