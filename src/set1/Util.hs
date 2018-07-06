761fe72a8249b0d0b7bd4f6b58722a77694cda10
Aniruddh Agarwal <aaniruddh99@gmail.com>
2018-07-05 20:02:12 -0400

    Implement xor for hex strings

--- src/set1/Util.hs
+++ src/set1/Util.hs
@@ -4,15 +4,33 @@ module Util where
 
 import Protolude
 
+import qualified Data.ByteString as B
 import qualified Data.ByteString.Base16 as Hex
 import qualified Data.ByteString.Base64 as B64
 
 {- Encoding/Decoding utilities -}
 
-hexToBase64 :: ByteString -> ByteString
-hexToBase64 = B64.encode . fst . Hex.decode
+hexDecode :: ByteString -> ByteString
+hexDecode = fst . Hex.decode
 
+-- Deserializes a string representing Hex and then serializes it to a
+-- string representing B64.
+hexToBase64 :: ByteString -> ByteString
+hexToBase64 = B64.encode . hexDecode
+
+-- Deserializes a string representing B64 and then serializes it to a
+-- string representing Hex.
 base64ToHex :: ByteString -> ByteString
 base64ToHex inp = Hex.encode $ case B64.decode inp of
                                  Right s -> s
                                  Left  _ -> ""
+
+{- XOR -}
+
+-- 'xor' for 'ByteString's
+bXor :: ByteString -> ByteString -> ByteString
+bXor x y = B.pack $ B.zipWith xor x y
+
+-- 'bXor' but wrapped with a Hex serializer/deserializer
+hXor :: ByteString -> ByteString -> ByteString
+hXor i1 i2 = Hex.encode $ (hexDecode i1) `bXor` (hexDecode i2)
