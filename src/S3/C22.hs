3edfd5c2dfb9d126ea0a6c820e7ea24c74d52f2c
Aniruddh Agarwal <aaniruddh99@gmail.com>
2018-07-17 04:29:48 -0400

    Fix challenge 22 implementation

--- src/S3/C22.hs
+++ src/S3/C22.hs
@@ -7,21 +7,31 @@ import Data.Word
 
 import Cipher.RNG
 
+-- seconds
+minTime :: Integral a => a
+minTime = 40
+
+-- seconds
+maxTime :: Integral a => a
+maxTime = 60
+
 getRandom :: IO Word32
-getRandom = do x <- randomRIO (40 :: Int, 1000)
-               threadDelay x
-               t <- (round . (* 1000)) <$> getPOSIXTime
+getRandom = do x <- randomRIO (minTime, maxTime)
+               threadDelay $ x * 1000000
+               t <- round <$> getPOSIXTime
                putStrLn $ "Generated seed: " ++ (show t)
                let rng = initRNG t
-               y <- randomRIO (40 :: Int,1000)
-               threadDelay y
+               y <- randomRIO (minTime, maxTime)
+               threadDelay $ y * 1000000
                pure $ fst $ getRand rng
 
 -- First output -> range of seeds
 bruteForceSeed :: Word32 -> [Word32] -> Word32
-bruteForceSeed o r = head $ filter (==o) $ map (fst . getRand . initRNG) r
+bruteForceSeed o r = snd . head $
+                     filter ((==o) . fst) $
+                     map (fst . getRand . initRNG >>= (,)) r
 
 runS3C22 :: IO ()
 runS3C22 = do r <- getRandom
-              t <- (round . (* 1000)) <$> getPOSIXTime
-              putStrLn $ "Bruteforced seed: " ++ (show $ bruteForceSeed r [t-5000000..t])
+              t <- round <$> getPOSIXTime
+              putStrLn $ "Bruteforced seed: " ++ (show $ bruteForceSeed r [(t-(3 * maxTime))..t])
