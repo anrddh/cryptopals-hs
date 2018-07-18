4618f340c34c4a0f01633104c906e62efa26eb01
Aniruddh Agarwal <aaniruddh99@gmail.com>
2018-07-17 21:22:30 -0400

    Challenge 25

--- src/Cipher/AES.hs
+++ src/Cipher/AES.hs
@@ -50,6 +50,15 @@ dCBC key iv ct
         plainOut  = dECB key cipherInp `bXor` iv
         nextBlock = dCBC key cipherInp (B.drop 16 ct)
 
+-- Performs CTR on a block
+-- Ignores everything after the 16th byte in the PlainText
+ctrBlock :: Key -> Nonce -> Int -> PlainText -> CipherText
+ctrBlock k n b p = eECB k ci `bXor` B.take 16 p
+  where c  = fromIntegral b                  -- Counter (Word64)
+        nB = toLazyByteString $ word64LE n   -- Nonce as a ByteString
+        cB = toLazyByteString $ word64LE c   -- Counter as a ByteString
+        ci = BL.toStrict $ nB `BL.append` cB -- Cipher input
+
 -- The Key must be 16 bytes; there's a 8 byte limit on both the Nonce
 -- and the Counter.
 eCTR :: Key -> Nonce -> Counter -> PlainText -> CipherText
--- src/S4/C25.hs
+++ src/S4/C25.hs
@@ -1,17 +1,50 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module S4.C25 where
 
+import Prelude hiding (break)
+
+import Data.ByteString (ByteString)
+import qualified Data.ByteString as B
 import Control.Lens
 import Data.Word
+import Data.Text.Encoding
+import Debug.Trace
+import Data.Maybe
 
 import Cipher.AES
+import Cipher.Rand
 import Util
+import Xor
 
--- A more efficient implementation would only decrypt/encrypt the
--- block we care about, but what the hell.
 edit :: Nonce -> Key -> Int -> CipherText -> Word8 -> CipherText
-edit n k pos ct b = eCTR k n 0 edited
-  where decrypted = dCTR k n 0 ct
-        edited    = decrypted & ix pos .~ b
+edit n k pos ct b = B.take blockStart ct `B.append`
+                    encrypted `B.append`
+                    B.drop (blockStart + 16) ct
+  -- eCTR k n 0 edited
+  where blockNum       = pos `div` 16
+        blockStart     = blockNum * 16
+        decryptedBlock = ctrBlock k n blockNum (B.drop blockStart ct)
+        edited         = decryptedBlock & ix (pos - blockStart) .~ b
+        encrypted      = ctrBlock k n blockNum edited
+
+editCiphertext :: (Int -> CipherText -> Word8 -> CipherText)
+               -> Int -- curr offset
+               -> CipherText
+               -> Word8
+               -> CipherText
+editCiphertext e i c w
+  | B.length c == i = c
+  | otherwise       = editCiphertext e (i + 1) (e i c w) w
+
+break :: (Int -> CipherText -> Word8 -> CipherText)
+      -> CipherText
+      -> PlainText
+break e c = c `bXor` editedCt `bXor` (charRepl (B.length c) 65)
+  where editedCt = editCiphertext e 0 c 65
 
 runS4C25 :: IO ()
-runS4C25 = undefined
+runS4C25 = do key  <- genKey
+              file <- fromJust . stripPad . dECB "YELLOW SUBMARINE" . d . right . decode . B64 <$> (B.readFile "25.txt")
+              putStrLn . show $ break (edit 0 key) (eCTR key 0 0 file)
+  where right (Right x) = x
